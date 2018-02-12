ad0b02244751a434c785214067365aad5d3d9fab
Gimin Moon <gmoon@usc.edu>
2018-02-11 19:05:32 -0800

    check for error code while using metadata on uploading images

--- DateCourse/DateCourse/addCourseItinerary.swift
+++ DateCourse/DateCourse/addCourseItinerary.swift
@@ -106,29 +106,31 @@ class addCourseItinerary: UIViewController, UITableViewDelegate, UITableViewData
                 let currentImage = cell.placeImageView.image
                 let storageImage = currentImage?.resizedTo1MB()!
                 let imageName = NSUUID().uuidString
-                self.photoIDs.append(imageName)
-                
+                //self.photoIDs.append(imageName)
+    
                 let storageRef = Storage.storage().reference().child("\(imageName).png")
                 if let uploadData = UIImagePNGRepresentation(storageImage!){
                     storageRef.putData(uploadData, metadata: nil, completion: { (metadata, error) in
-                        if error != nil{
-                            print(error!)
+                        
+                        if let error = error{
+                            print(error)
+                            print("printed eror")
                             return
                         }
+                        
                         guard let metadata = metadata else {
                             print("error")
                             return
                         }
-                        print("hu...")
                         // Metadata contains file metadata such as size, content-type, and download URL.
-                        let downloadURL = metadata.downloadURL()?.absoluteString
-                        //self.photoIDs.append(downloadURL!)
-                        print("adding..")
-                        //get each photos downloadable image URL
-//                        if let imageURL = downloadURL?.absoluteString{
-//                            self.photoIDs.append(imageURL)
-//                            print(imageURL)
-//                        }
+                        if let downloadURL = metadata.downloadURL()?.absoluteString{
+                            //store the downloadable url's from each saved photo
+                            self.photoIDs.append(downloadURL)
+                        }
+                        else
+                        {
+                            print("it is nillll")
+                        }
                         print(metadata)
                         })
                     }
@@ -138,19 +140,19 @@ class addCourseItinerary: UIViewController, UITableViewDelegate, UITableViewData
             print("not valid form")
             return
         }
-        //save each "course" to firebase under the current user.
-//        let course = CourseData.init(title: title, intro: intro, locations: addCourseMap.locations, images: self.photos, descriptions : self.descriptions)
-        let course = CourseData()
+            
+        //upload course to FireBase under "courses"
         let user = Auth.auth().currentUser
         guard let uid = user?.uid else{
             return
         }
+        
         let ref = Database.database(url: "https://datecourse-app.firebaseio.com/").reference()
-            let userReference = ref.child("courses").childByAutoId()
-            // need to fix saving locations
-            print("photoID has : \(self.photoIDs.count) items ")
-            let addingCourse = ["user": uid, "title": title, "intro": intro, "locations": title, "images": self.photoIDs, "descriptions" : self.descriptions] as [String : Any]
-            userReference.updateChildValues(addingCourse, withCompletionBlock: {(err,ref) in
+        let userReference = ref.child("courses").childByAutoId()
+        // need to fix saving locations
+        print("photoID has : \(self.photoIDs.count) items ")
+        let addingCourse = ["user": uid, "title": title, "intro": intro, "locations": title, "images": self.photoIDs, "descriptions" : self.descriptions] as [String : Any]
+        userReference.updateChildValues(addingCourse, withCompletionBlock: {(err,ref) in
             if err != nil {
                 print(err as Any)
                 return
@@ -159,7 +161,6 @@ class addCourseItinerary: UIViewController, UITableViewDelegate, UITableViewData
         })
     
         //invoke save functionality to clear fields
-        self.onSave?(course)
         self.clear()
         self.dismiss(animated: true, completion: nil)
         
